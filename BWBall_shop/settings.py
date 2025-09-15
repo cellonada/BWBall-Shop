3bbde509f9a525a42fdb6a0b1f2035169eaf84c3
cellonada <cellonada06@gmail.com>
2025-09-16 00:19:58 +0700

    Tugas 3 Complete

--- BWBall_shop/settings.py
+++ BWBall_shop/settings.py
@@ -148,3 +148,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#nambahin komen biar bisa push lagi karna error sebelumnyaaa
